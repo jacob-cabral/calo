f2a621f0a02dabe31c377b9ed7c20e3e6f9d680d
Jacob Cabral <jacob.cabral@terceiro.rnp.br>
2024-07-30 22:47:01 -0300

    fix: obtenção do prefixo de rede IPv4

--- deploy/bind9.sh
+++ deploy/bind9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Implantação do serviço DNS (Bind9)."
-prefixlen=$(ip -json address show $ifname | jq -r .[].addr_info[].prefixlen)
+prefixlen=$(ip -json address show $ifname | jq -r '.[].addr_info[] | select(.family = "inet").prefixlen')
 cidr=$ip/$prefixlen
 ipReverso=$(echo $ip | sed --expression='s/^\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\)\.\([0-9]\{1,3\}\).\+$/\3.\2.\1/')
 octeto=$(echo $ip | sed --expression='s/^\([0-9]\{1,3\}\).\+$/\1/')
