152afb94f205a92351b40f2783a6b3eb856c934b
Jacob Cabral <jacob.cabral@terceiro.rnp.br>
2024-07-31 23:48:32 -0300

    fix: importação da chave PGP e configuração do repositório APT do Docker

--- lib/docker.sh
+++ lib/docker.sh
@@ -5,7 +5,19 @@ set -e
 # Instalação do Docker.
 if test -z "$(which docker)"
 then
-sudo apt install --yes docker docker-buildx docker-clean docker-compose-v2 docker-doc docker.io
+# Importação da chave PGP oficial do Docker.
+sudo apt install ca-certificates curl --yes
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Configução do repositório APT do Docker.
+cat <<- EOF | sudo tee /etc/apt/sources.list.d/docker.list
+deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable
+EOF
+# Intalação da versão mais recente do Docker.
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Inclusão do usuário atual como membro do grupo do Docker.
 sudo usermod --append --groups docker $USER
 cat <<- EOF | sudo tee /etc/docker/daemon.json
 {
