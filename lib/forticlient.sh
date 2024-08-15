92f9c6616b3d2bd129b82817883ecba2eafcc0d3
Jacob Cabral <jacob.cabral@terceiro.rnp.br>
2024-08-15 01:15:45 -0300

    fix: acréscimo do repositório LTS da dependência libappindicator

--- lib/forticlient.sh
+++ lib/forticlient.sh
@@ -5,6 +5,14 @@ set -e
 # Instalação do FortiClient.
 if test -z "$(which forticlient)"
 then
+  jammySources=/etc/apt/sources.list.d/jammy.list
+
+  if ! test -f ${jammySources}
+  then
+    echo "deb http://archive.ubuntu.com/ubuntu jammy main universe" | sudo tee ${jammySources}
+    sudo apt update
+  fi
+
   installer=/tmp/forticlient-vpn.deb
   curl --location --output $installer https://links.fortinet.com/forticlient/deb/vpnagent
   sudo dpkg --install $installer
