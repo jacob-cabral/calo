2aa139fdce90ba50ec2e4c5b2cd3b4ee0cd5cd65
Jacob Cabral <jacob.cabral@terceiro.rnp.br>
2024-08-16 09:55:58 -0300

    fix: acréscimo da mensagem de sucesso

--- lib/forticlient.sh
+++ lib/forticlient.sh
@@ -16,6 +16,7 @@ then
   sudo apt install --fix-broken --yes
   rm $installer
   unset installer
+  echo "O FortiClient foi instalado com sucesso."
 else
   echo "O FortiClient já está instalado."
 fi
