a55d8ba20809c18da23f2da557113f3f81fbea87
Jacob Cabral <jacob.cabral@terceiro.rnp.br>
2024-08-16 10:12:45 -0300

    fix: ajustes da instalação e da configuração do SDKMAN!

--- lib/sdkman.sh
+++ lib/sdkman.sh
@@ -5,10 +5,9 @@ set -e
 # Instalação do SDKMAN.
 if [[ ! -d "$SDKMAN_DIR" ]] || [[ ! -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]]
 then
-curl -s "https://get.sdkman.io" | bash
-cat << EOF >> ~/.bashrc
-
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+curl -s "https://get.sdkman.io?rcupdate=false" | bash
+cat << EOF | sudo tee /etc/profile.d/sdkman.sh
+# SDKMAN! Configuration
 export SDKMAN_DIR="\$HOME/.sdkman"
 [[ -s "\$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "\$SDKMAN_DIR/bin/sdkman-init.sh"
 EOF
