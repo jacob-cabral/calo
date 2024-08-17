1a40148453792b96b79e111079808925c0707f41
Jacob Cabral <jacob.cabral@terceiro.rnp.br>
2024-08-17 02:11:26 -0300

    fix: uso de getter para sanar conflito de variável homônima

--- util/reboot-needed.sh
+++ util/reboot-needed.sh
@@ -2,12 +2,14 @@
 # Interrompe a execução em caso de erro.
 set -e
 
-# Define o arquivo de marcação da necessidade de reiniciar o sistema operacional.
-file=/tmp/.reboot.needed
+# Obtém o arquivo de marcação da necessidade de reiniciar o sistema operacional.
+getMarkupFile() {
+  echo -n /tmp/.reboot.needed
+}
 
 # Verifica a necessidade de reiniciar o sistema operacional.
 isRebootNeeded() {
-  if test -f "${file}"
+  if test -f "$(getMarkupFile)"
   then
     echo "É necessário reiniciar o sistema operacional."
     exit 1
@@ -16,5 +18,5 @@ isRebootNeeded() {
 
 # Define a necessidade de reiniciar o sistema operacional.
 setRebootNeeded() {
-  touch "${file}"
+  touch "$(getMarkupFile)"
 }
