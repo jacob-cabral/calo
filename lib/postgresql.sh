9f1886e9ab14a5d0334e38351c8797257ce75572
Jacob Cabral <jacob.cabral@terceiro.rnp.br>
2024-11-10 17:01:41 -0300

    refactor: instalação da versão 14 do PostgreSQL Client

--- lib/postgresql.sh
+++ lib/postgresql.sh
@@ -5,7 +5,10 @@ set -e
 # Instalação dos utilitários clientes do PostgreSQL.
 if test -z "$(which psql)"
 then
-  sudo apt install postgresql-client libpq-dev --yes
+  sudo curl --silent https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor --output /etc/apt/trusted.gpg.d/postgresql.gpg
+  sudo sh -c 'echo "deb[arch=amd64 signed-by=/etc/apt/trusted.gpg.d/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  sudo apt update
+  sudo apt install postgresql-client-14 libpq-dev --yes
 else
   echo "Os utilitários clientes do PostgreSQL já estão instalados."
 fi
