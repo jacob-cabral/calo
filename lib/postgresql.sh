debfa1adc4fed5fcbdeb947d04ba038d11767486
Jacob Cabral <jacob.cabral@terceiro.rnp.br>
2024-12-11 15:23:39 -0300

    refactor: atualização do repositório do PostgreSQL

--- lib/postgresql.sh
+++ lib/postgresql.sh
@@ -6,7 +6,7 @@ set -e
 if test -z "$(which psql)"
 then
   sudo curl --silent https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor --output /etc/apt/trusted.gpg.d/postgresql.gpg
-  sudo sh -c 'echo "deb[arch=amd64 signed-by=/etc/apt/trusted.gpg.d/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt/ noble-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   sudo apt update
   sudo apt install postgresql-client-14 libpq-dev --yes
 else
